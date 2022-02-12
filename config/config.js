d1bc9571e36eadcb36f47039d79c589fa99b1013
ruckyTheWebDesigner <rukewejoseph77@gmail.com>
2022-02-12 23:52:54 +0700

    removed .env

--- config/config.js
+++ config/config.js
@@ -1,13 +1,12 @@
-require('dotenv').config()
 // Import the functions you need from the SDKs you need
 const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.FIREBASE_APP_ID,
-  measurementId: process.env.FIREBASE_MEASUREMENT_ID,
+  apiKey: 'AIzaSyDv07vt2SSZLxnS7ZU18plSnk6mRqE1kJs',
+  authDomain: 'firstapp-9e997.firebaseapp.com',
+  projectId: 'firstapp-9e997',
+  storageBucket: 'firstapp-9e997.appspot.com',
+  messagingSenderId: '899053817897',
+  appId: '1:899053817897:web:61d7ecece206eae2114c38',
+  measurementId: 'G-D7YJSQRMJ1',
 }
 
 // Initialize Firebase
